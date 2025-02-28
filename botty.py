b0601c06685469c72308e96d86aedafafadeaaf0
ify-crypto <ifeyinwagraceokafor@yahoo.com>
2025-02-28 11:41:29 +0100

    Update botty.py

--- botty.py
+++ botty.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('ignore')
 
 
 # Download required NLTk data
-# nltk.download('punkt')
+nltk.download('punkt')
 # nltk.download('wordnet')
 # nltk.download('stopwords')
 # nltk.download('punkt_tab')
